7ef616a489b605effbbabaf1066829650577fd51
Habibur Rahman <hr19392@gmail.com>
2021-06-08 17:41:13 +0200

    tested pcell in sub network

---
+++ HC180_TSMC65_wrk/workspace.ads
@@ -0,0 +1,38 @@
+<ADSWorkspace Revision="4" Version="100">
+    <Workspace Name="">
+        <LibraryDefs Name="lib.defs" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Preferences Name="HC180_TSMC65_lib_lay.prf" />
+        <Preferences Name="HC180_TSMC65_lib_sch.prf" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <Library Name="ads_standard_layers" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_builtin" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_pelib" />
+        <Library Name="HC180_TSMC65_lib" />
+        <Substrate Name="HC180_TSMC65_lib:CRN65LP.subst" />
+        <Substrate Name="HC180_TSMC65_lib:CRN65LP_reduced.subst" />
+        <WrkAel Name="boot.ael" />
+        <WrkAel Name="HC180.ael" />
+        <Log Name="search_history.log" />
+        <Preferences Name="ads_builtin_lay.prf" />
+        <Cell Name="HC180_TSMC65_lib:HC180" />
+        <Cell Name="HC180_TSMC65_lib:HC180_sub" />
+    </Workspace>
+</ADSWorkspace>
